1ab4af252e29784a2633ad70498e27f8e74161e6
SanteriMikkola <santerimikkola04@gmail.com>
2021-09-29 22:40:44 +0300

    Tehtävä 15 osa 1

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+   console.log("Got a GET request for the homepage");
+   res.send('Hello GET');
+})
+
+app.post('/', function (req, res) {
+   console.log("Got a POST request for the homepage");
+   res.send('Hello POST');
+})
+
+app.delete('/del_user', function (req, res) {
+   console.log("Got a DELETE request for /del_user");
+   res.send('Hello DELETE');
+})
+
+app.get('/list_user', function (req, res) {
+   console.log("Got a GET request for /list_user");
+   res.send('Page Listing');
+})
+
+app.put('/user', function (req, res) {
+    res.send('Got a PUT request at /user')
+  })
+
+app.listen(3000, () => {
+   
+   console.log(`Example app listening at http://localhost:${3000}`)
+})
